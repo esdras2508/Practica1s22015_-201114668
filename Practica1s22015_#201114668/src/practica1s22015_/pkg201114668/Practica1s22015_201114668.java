cfe970d76c08df993bad77d7c7b55c3cf3c25432
esdras2508 <esdras2508@gmail.com>
2015-08-05 19:59:55 -0600

    Creación del Proyecto
    se inicio con la sincronización del proyecto

---
+++ Practica1s22015_#201114668/src/practica1s22015_/pkg201114668/Practica1s22015_201114668.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1s22015_.pkg201114668;
+
+/**
+ *
+ * @author esdras
+ */
+public class Practica1s22015_201114668 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
